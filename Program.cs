f50b89ee03d48d745640efa55ede05e32961cf0b
Nick Chen <chenyahua2012@hotmail.com>
2023-12-23 16:03:03 -0800

    new solution for a do-while question

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Please input a integer value between 5 and 10.");
+string? input;
+int finalNumber = 0;
+bool? validNumber;
+do{
+    int inputNumber = 0;
+    input = Console.ReadLine();
+    validNumber = int.TryParse(input, out inputNumber);
+
+    if(validNumber == true)
+    {
+        if(inputNumber<5||inputNumber>10)
+        {
+            Console.WriteLine($"You entered {inputNumber}. Please enter a number between 5 and 10.");
+            continue;
+        }else{
+            finalNumber = inputNumber;
+        }
+    }else{
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        continue;
+    }                   
+
+}while(finalNumber==0);
+
+Console.WriteLine($"Your input value ({finalNumber}) has been accepted.");
