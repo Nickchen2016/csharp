08e127ddc2491b321a1218e5596c50348abb77b0
Nick Chen <chenyahua2012@hotmail.com>
2023-12-23 16:07:13 -0800

    question has been added to the top page

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿
+﻿/* Code project 1 - write code that validates integer input
+Here are the conditions that your first coding project must implement:
+
+. Your solution must include either a do-while or while iteration.
+
+. Before the iteration block: your solution must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+
+. Inside the iteration block:
+    Your solution must use a Console.ReadLine() statement to obtain input from the user.
+    Your solution must ensure that the input is a valid representation of an integer.
+    If the integer value isn't between 5 and 10, your code must use a Console.WriteLine() statement to prompt the user for an integer value between 5 and 10.
+    Your solution must ensure that the integer value is between 5 and 10 before exiting the iteration.
+    
+. Below (after) the iteration code block: your solution must use a Console.WriteLine() statement to inform the user that their input value has been accepted.*/
+
+
+
+
 Console.WriteLine("Please input a integer value between 5 and 10.");
 string? input;
 int finalNumber = 0;
